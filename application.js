981260d79edadbdfda823001837f3d140085361b
heatherjang <heather.m.jang@gmail.com>
2015-03-11 22:19:24 +0000

    JS: Query value accessible

--- application.js
+++ application.js
@@ -1,4 +1,10 @@
-(function() {
+$(function() {
+
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    var query = $("#input-city").val();
+    console.log(query);
+  })
 
 
 });
