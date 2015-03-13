468c31fcb8c3fedb51d18535777e60c21fb2bd24
heatherjang <heather.m.jang@gmail.com>
2015-03-13 20:25:48 +0000

    Search returns location, current weather and temperature c

--- application.js
+++ application.js
@@ -12,13 +12,13 @@ $(function() {
         $.each(data, function(i, cities) {
           $.each(cities, function(i, city) {
             var listItem = $('<li>').addClass('list-item');
-            listItem.html(city['name']);
+            listItem.html(city.name);
             $('#search-results').append(listItem);
           });
         });
       },
       error: function(){
-        console.log("Error!");
+        console.log("Form error!");
       },
     });
 
@@ -39,17 +39,30 @@ $(function() {
     var query = $('#form-input').val();
     var country = query.split(',')[1];
     var city = query.split(',')[0];
-    console.log(country);
-    console.log(city);
     $.ajax({
       url: "http://api.wunderground.com/api/7a27f05a0037149f/conditions/q/" + country + '/' + city + ".json",
       type: 'GET',
       dataType: 'jsonp',
       success: function(data){
-        console.log(data);
+        $('#results').empty();
+        $('#current').empty();
+        $('#forecast').empty();
+        if (data.current_observation){
+          var current_obs = data.current_observation;
+          console.log(current_obs);
+          var display_loc = current_obs.display_location;
+          var city = display_loc.city;
+          var state = display_loc.state_name;
+          var condition = current_obs.weather;
+          var temp_c = current_obs.temp_c;
+          var feelslike_c = current_obs.feelslike_c;
+          $('#current').html(city+ " " +state+ " " +condition+ " " +temp_c+ " " +feelslike_c);
+        } else {
+          console.log('no data found for that city')
+        }
       },
       error: function(){
-        console.log('Error!')
+        console.log('search submission error')
       },
     });
 
